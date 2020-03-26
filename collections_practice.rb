48194eb2375ce65d536d12955dcecb027707aa32
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 20:28:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -115,9 +115,9 @@
   array = ["hand","feet", "knee", "table"]
   def add_s(array)
     array.each_with_index.collect{|element, index| 
-      if !index == 1
-      element<<("s")
-      end 
+      #if !index == 1
+        element<<("s")
+      #end 
     }
   end 
   
